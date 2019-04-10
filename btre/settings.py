82b804aea78258ec6eba97bac1a88d1d26178931
Percev Mihail <arthasmontano@mail.ru>
2019-04-10 20:15:30 +0300

    Added listings (and single list), added search. Modified "navbar" in partials and "settings", "urls" in btre directory.

--- btre/settings.py
+++ btre/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+
 ]
 
 MIDDLEWARE = [
