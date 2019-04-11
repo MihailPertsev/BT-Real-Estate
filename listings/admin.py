a79bcdc839f7a40b68b8d678240db0b02647fbf2
Percev Mihail <arthasmontano@mail.ru>
2019-04-11 16:50:44 +0300

    Added admin. Added listings and realtors into admin. Added media folder. Uploaded admin with media.

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display =('title','photo_main','price')
+
+
+admin.site.register(Listing, ListingAdmin)
+
