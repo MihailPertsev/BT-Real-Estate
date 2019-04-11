a4388636de3cd4323736ef01c6eb0229c9401e06
Percev Mihail <arthasmontano@mail.ru>
2019-04-11 20:20:24 +0300

    Customize admin area. Added search and filter options for listings and realtors. Added RealtorAdmin class.

--- listings/admin.py
+++ listings/admin.py
@@ -3,8 +3,11 @@ from .models import Listing
 
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display =('title','photo_main','price')
-
-
+    list_display =('id','is_published','title','price','list_date','realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title','description','address','city','state','zipcode','price')
+    list_per_page = 25
 admin.site.register(Listing, ListingAdmin)
 
