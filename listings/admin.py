e44b58f55e06d4cf676607de3cd702aeedb15431
Percev Mihail <arthasmontano@mail.ru>
2019-04-19 15:57:30 +0300

    Fixed seller of the month. Slightly changed admin.py in listings/

--- listings/admin.py
+++ listings/admin.py
@@ -5,10 +5,10 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
     list_display =('id','is_published','title','price','list_date','realtor')
     list_display_links = ('id', 'title')
-    list_filter = ('realtor',)
+    list_filter = ('realtor','price')
     list_editable = ('is_published',)
     search_fields = ('title','description','address','city','state','zipcode','price')
-    list_per_page = 25
+    list_per_page = 15
 
 
 admin.site.register(Listing, ListingAdmin)
